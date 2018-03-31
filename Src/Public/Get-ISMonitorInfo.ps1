e7fa2cf8d180e340107c451280448fc2aa43e2d9
Rob Derickson <rob.derickson@erickson.com>
2018-03-31 17:05:52 -0400

    Added Verbose and Debug messages to Get-ISMonitorInfo

--- Src/Public/Get-ISMonitorInfo.ps1
+++ Src/Public/Get-ISMonitorInfo.ps1
@@ -12,11 +12,16 @@ function Get-ISMonitorInfo {
         foreach ($computer in $ComputerName) {
             try {
                 $ComputerInfo = Get-CimInstance -ComputerName $computer -ClassName Win32_ComputerSystem -ErrorAction Stop
+
+                Write-Verbose -Message "Retrieved computer data from Win32_ComputerSystem for computer $computer."
+                Write-Debug -Message $computer
             } catch {
-                Write-Error "Unable to query Win32_ComputerSystem on computer $computer"
+                Write-Error -Message "Unable to query Win32_ComputerSystem on computer $computer.`n($_.Exception.Message)"
             }
+
             try {
                 $Monitors = Get-CimInstance -ComputerName $computer -ClassName wmiMonitorID -Namespace root\wmi -ErrorAction Stop
+                
                 foreach ($monitor in $Monitors) {
                     $info = @{
                         ComputerName = $computer
@@ -27,8 +32,13 @@ function Get-ISMonitorInfo {
                     if ($monitor.UserFriendlyName -ne $null) {
                         $info.MonitorType = [System.Text.Encoding]::ASCII.GetString($monitor.UserFriendlyName)
                     }
+                
                     $object = New-Object -TypeName PSObject -Property $info
+                
                     Write-Output $object
+
+                    Write-Verbose -Message "Retrieved monitor data from wmiMonitorID for computer $computer."
+                    Write-Debug -Message $info
                 }
             } catch {
                 Write-Error "Unable to query wmiMonitorID on computer $computer. $($_.Exception.Message)"
